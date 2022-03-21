6419f1e1017c580fde4c10249a33142107f08cef
SE34 <everdens@student.ubc.ca>
2022-03-21 07:34:15 -0700

    updated user list and home page

--- app/forms.py
+++ app/forms.py
@@ -8,15 +8,13 @@ from django.contrib.auth.models import User
 #          model = User
 #          fields = ['name', 'username', 'email', 'password1', 'password2',]
 
-
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
         exclude = ['host', 'participants']
 
-
 class UserForm(ModelForm):
      class Meta:
         model =User
-        fields  = ['username']
+        fields  = ['username','email']
